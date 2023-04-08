86485c419124d6306c38e78b9afdb732b847ce77
Shaxboz <shaxbozali9@gmail.com>
2023-04-08 15:18:47 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useGlobalContext } from './context'
+
+
+import Navbar from './Navbar'
+import CartContainer from './CartContainer'
+
+import "./App.css";
+
+
+
+function App() {
+  const { loading } = useGlobalContext()
+  if (loading) {
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export default [
+	{
+		id: 1,
+		title: 'Samsung Galaxy S7',
+		price: 599.99,
+		img: 'https://giperskidka.ru/assets/images/products/640x/samsung_galaxy_s7_original.jpg',
+		amount: 1
+	},
+	{
+		id: 2,
+		title: 'google pixel ',
+		price: 499.99,
+		img: 'https://www.juzaphoto.com/shared_files/recensioni/google_pixel_xl.jpg',
+		amount: 1
+	},
+	{
+		id: 3,
+		title: 'Xiaomi Redmi Note 2',
+		price: 699.99,
+		img: 'https://www.eeplatz.com/328-large_default/xiaomi-redmi-note-2-smartphone-55-inch-2gb-32gb-white.jpg',
+		amount: 1
+	}
+];
